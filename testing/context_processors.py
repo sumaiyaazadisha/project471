b732845ec68e5d60d8c167677c6510584271749d
sumaiyaazadisha <sumaiya.azad1469@gmail.com>
2024-04-22 11:47:55 +0600

    .

--- testing/context_processors.py
+++ testing/context_processors.py
@@ -1,13 +1,11 @@
 from .models import combo,Product
 
 def three_combos(request):
-    # Retrieve any three combos
     three_combos = combo.objects.all()[:3]
     return {'three_combos': three_combos}
 
 
 def top_products(request):
-    # Retrieve the top three highest-rated products
     top_products = Product.objects.order_by('-rating')[:3]
     return  {'top_products': top_products}
 
